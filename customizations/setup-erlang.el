cb553d018402690bb9adeb91e4b4f16eb54de52f
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2017-05-01 22:49:44 +0200

    .gitignore, make setup-erlang work on OS X

--- customizations/setup-erlang.el
+++ customizations/setup-erlang.el
@@ -1,6 +1,23 @@
-(setq load-path (cons "/usr/lib/erlang/lib/tools-2.9.1/emacs" 
-load-path))
+(cond 
+ ((eq system-type 'darwin)
+  ;;
+  ;; OS X settings
+  ;;
+  (setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.9.1/emacs"
+                        load-path))
+  (setq erlang-root-dir "/usr/local/lib/erlang")
+  (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path)))
 
-(setq erlang-root-dir "/usr/lib/erlang")
-(setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
+ ((eq system-type 'gnu/linux)
+  ;;
+  ;; Linux settings
+  ;;
+  (setq load-path (cons "/usr/lib/erlang/lib/tools-2.9.1/emacs"
+                        load-path))
+  (setq erlang-root-dir "/usr/lib/erlang")
+  (setq exec-path (cons "/usr/lib/erlang/bin" exec-path)))
+
+)
+
+
 (require 'erlang-start)
